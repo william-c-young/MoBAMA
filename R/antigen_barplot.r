73192ac9b3f21feff31ae80136f3f5345e191ffd
wyoung <wyoung@fhcrc.org>
2018-04-30 12:14:42 -0700

    Fix antigen barplot to work with multiple timepoints.

--- R/antigen_barplot.r
+++ R/antigen_barplot.r
@@ -1,6 +1,7 @@
 #' Barplot of antigen means
 #' TODO: add more options
-#'         colors (by antigen group, user-specified)
+#'       tp filtering
+#'       colors (by antigen group, user-specified)
 #'       make the plot cleaner/nicer
 #'       not hard code axis labels?
 #'
@@ -13,10 +14,18 @@
 #' @export
 antigen_barplot <- function(result,
                             includeIntervals = F) {
-    barPlot <- ggplot(result$mu_ag, aes(ag, mu_ag)) +
-        geom_bar(stat="identity", alpha = 0.7) +
-        labs(x = "Antigen", y = "Mean") +
-        coord_flip()
+    barPlot <- NULL
+    if ( length(unique(result$mu_ag$tp)) > 1 ) {
+        barPlot <- ggplot(result$mu_ag, aes(paste(ag, tp), mu_ag, fill = factor(tp))) +
+            geom_bar(stat="identity", alpha = 0.9) +
+            labs(x = "Antigen", y = "Mean") +
+            coord_flip()
+    } else {
+        barPlot <- ggplot(result$mu_ag, aes(ag, mu_ag)) +
+            geom_bar(stat="identity", alpha = 0.9) +
+            labs(x = "Antigen", y = "Mean") +
+            coord_flip()
+    }
     if (includeIntervals) {
         barPlot <- barPlot +
             geom_point(size = 3, color = "gray30") +
